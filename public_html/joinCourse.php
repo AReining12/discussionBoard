6b1b684e2e4ef24f521d3e22a165d1e13c1ebd60
mingchenju <59490061+mingchenju@users.noreply.github.com>
2023-12-11 09:00:12 -0500

    Update joinCourse.php

--- public_html/joinCourse.php
+++ public_html/joinCourse.php
@@ -1,26 +1,37 @@
 <?php
 // joinCourse.php
+// Mingchen Ju 260864282
 
-session_start(); // 确保会话已经启动
-header('Content-Type: application/json'); // 设置响应的内容类型为 JSON
+session_start(); // Ensure the session is started
+header('Content-Type: application/json'); // Set the response content type to JSON
 
-include 'db_connect.php'; // 包含数据库连接
+include 'db_connect.php'; // Include database connection
 
-// 检查用户是否登录
+// Check if the user is logged in
 if (!isset($_SESSION['user_id'])) {
     echo json_encode(['success' => false, 'error' => 'User not logged in']);
     exit;
 }
 
-$userId = $_SESSION['user_id']; // 获取当前登录用户的 ID
+$userId = $_SESSION['user_id']; // Get the ID of the currently logged-in user
 
-// 从请求中获取 boardId
+// Retrieve boardId from the request
 $data = json_decode(file_get_contents('php://input'), true);
 $boardId = $data['boardId'];
 
-// 检查用户是否已经申请加入该讨论板
+// First, check if the user is already a member of the board
+$stmt = $conn->prepare('SELECT * FROM board_users WHERE user_id = ? AND board_id = ?');
+$stmt->bind_param('ii', $userId, $boardId);
+$stmt->execute();
+$result = $stmt->get_result();
+if ($result->num_rows > 0) {
+    echo json_encode(['success' => false, 'error' => 'User is already a member of this board.']);
+    exit;
+}
+
+// Then, check if the user has already requested to join this board
 $stmt = $conn->prepare('SELECT * FROM board_applicants WHERE user_id = ? AND board_id = ?');
-$stmt->bind_param('ii', $userId, $boardId); // 'ii' 表示两个参数都是整型
+$stmt->bind_param('ii', $userId, $boardId);
 $stmt->execute();
 $result = $stmt->get_result();
 if ($result->num_rows > 0) {
@@ -28,13 +39,13 @@ if ($result->num_rows > 0) {
     exit;
 }
 
-// 将用户添加到 board_applicants 表中
+// Add the user to the board_applicants table
 $stmt = $conn->prepare('INSERT INTO board_applicants (user_id, board_id) VALUES (?, ?)');
 $stmt->bind_param('ii', $userId, $boardId);
 if ($stmt->execute()) {
     echo json_encode(['success' => true]);
 } else {
-    // 记录错误信息到服务器的错误日志
+    // Log error information to the server's error log
     error_log('Database error: ' . $stmt->error);
     echo json_encode(['success' => false, 'error' => 'An error occurred while processing your request.']);
 }
