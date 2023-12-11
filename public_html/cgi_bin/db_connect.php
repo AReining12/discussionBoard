5bf6e0e568df5cb37bc051fa2500774f600ef434
Mikhail Lavrenov <mikhail.lavrenov04@gmail.com>
2023-12-11 14:07:49 -0500

    Security feature
    Added forbidden.php which shows a message to users accessing pages in cgi_bin
    without being authenicated. To use, add:
    
    include('forbidden.php');
    
    below the line
    
    include('session_start.php');
    
    of any script you want to protect.

--- public_html/cgi_bin/db_connect.php
+++ public_html/cgi_bin/db_connect.php
@@ -4,16 +4,16 @@
 // Included in files that requre a database connection
 
 // localhost
-// $host = 'localhost';
-// $dbname = 'comp307';
-// $dbusername = 'root'; 
-// $dbpassword = ''; 
+$host = 'localhost';
+$dbname = 'comp307';
+$dbusername = 'root'; 
+$dbpassword = ''; 
 
 // SOCS
-$host = 'mysql.cs.mcgill.ca';
-$dbname = '2023fall-comp307-mlavre1';
-$dbusername = 'mlavre1'; 
-$dbpassword = '4zWW$f#5XR@FFdJgwX6h3ihd'; 
+// $host = 'mysql.cs.mcgill.ca';
+// $dbname = '2023fall-comp307-mlavre1';
+// $dbusername = 'mlavre1'; 
+// $dbpassword = '4zWW$f#5XR@FFdJgwX6h3ihd'; 
 
 // Create connection
 $conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
