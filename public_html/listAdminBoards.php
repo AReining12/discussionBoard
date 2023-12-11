1ffa5a6c34b3b8fa9a5b04347e0e882acc7e6936
mingchenju <59490061+mingchenju@users.noreply.github.com>
2023-12-11 17:16:05 -0500

    Update listAdminBoards.php

--- public_html/listAdminBoards.php
+++ public_html/listAdminBoards.php
@@ -6,21 +6,23 @@ header('Content-Type: application/json');
 
 include 'db_connect.php';
 
-// 检查是否有用户登录
+// Check if a user is logged in
 if (!isset($_SESSION['user_id'])) {
+    // Respond with an error if no user is logged in
     echo json_encode(['success' => false, 'error' => 'User not logged in']);
     exit;
 }
-
+// Get the user ID from the session
 $userId = $_SESSION['user_id'];
 
-// 查询用户作为管理员的所有讨论板
+// Prepare a SQL statement to select all boards where the user is an admin
 $stmt = $conn->prepare('SELECT b.board_id, b.board_name FROM boards b INNER JOIN board_users bu ON b.board_id = bu.board_id WHERE bu.user_id = ? AND bu.is_board_admin = 1');
 $stmt->bind_param('i', $userId);
 $stmt->execute();
+// Fetch the result as an associative array
 $boards = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
 
-// 不再需要检查 'is_staff' 字段，因为我们已经通过 'is_board_admin' 字段获取了管理员身份
+// Respond with the list of boards where the user is an admin
 echo json_encode(['success' => true, 'boards' => $boards]);
 
 $stmt->close();
