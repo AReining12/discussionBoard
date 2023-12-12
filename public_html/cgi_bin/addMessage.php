b128aff7fef69ca1dac8c207938a7d675ce0349f
AReining12 <97750589+AReining12@users.noreply.github.com>
2023-12-11 22:46:54 -0500

    Update addMessage.php

--- public_html/cgi_bin/addMessage.php
+++ public_html/cgi_bin/addMessage.php
@@ -2,7 +2,7 @@
     <head>
     </head>
     <body>
-    <!--Form to add or remove channels, Anna Reining, 260885420-->
+    <!--Form to add or remove channels-->
     <form name="input" action="messageController.php" method="post" autocomplete="on">
             <!--Hidden field to determine whether form is register or login-->
             <input type="hidden" name="action" value="add_message">
@@ -10,7 +10,7 @@
             <fieldset>
                 <b id="message">Message Content</b> <input type="text" name="message"> <br />
                 <b id="channel">Channel</b> <input type="text" name="channel"> <br />
-                <!--Get board id from $_POST variable boardname-->
+                
 
             </fieldset>
 
