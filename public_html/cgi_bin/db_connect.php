6dbbe94decefe00647ccbc6328009d999f30213e
Mikhail Lavrenov <mikhail.lavrenov04@gmail.com>
2023-12-11 23:46:00 -0500

    Cleaned up repo

--- public_html/cgi_bin/db_connect.php
+++ public_html/cgi_bin/db_connect.php
@@ -9,11 +9,7 @@ $dbname = 'comp307';
 $dbusername = 'root'; 
 $dbpassword = ''; 
 
-// SOCS
-// $host = 'mysql.cs.mcgill.ca';
-// $dbname = '2023fall-comp307-mlavre1';
-// $dbusername = 'mlavre1'; 
-// $dbpassword = '4zWW$f#5XR@FFdJgwX6h3ihd'; 
+
 
 // Create connection
 $conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
--- public_html/cgi_bin/index.php
+++ public_html/cgi_bin/index.php
@@ -1,15 +1,3 @@
-<html>
-    <head>
-    </head>
-    <body>
-        <a href="login.php">Login</a>
-        <a href="registration.php">Register</a>
-        <?php
-        // Anna Reining, 260885420
-        // Publicly accessible landing page with links to registration
-        // and login
-        
-        ?>
-
-    </body>
-</html>
+<?php
+    header("Location: ../landingpage.html");
+?>
